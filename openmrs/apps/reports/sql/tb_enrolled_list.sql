ef22e6e935b2f32dfd50a1356ce5224f59301b87
Samuel Masoebe <masoebesamuel@gmail.com>
2020-02-25 07:57:57 +0200

    Tb Relapsed and new list

--- openmrs/apps/reports/sql/tb_enrolled_list.sql
+++ openmrs/apps/reports/sql/tb_enrolled_list.sql
@@ -1,11 +1,12 @@
-   (SELECT  patientIdentifier AS "Patient Identifier", patientName AS "Patient Name", Age , Gender, age_group,TB_Treatment_History
+(SELECT patientIdentifier AS "Patient Identifier", patientName AS "Patient Name", Age , Gender, age_group, 'New Patient' AS 'TB_Treatment_History'
 							 
 					FROM
+					
 									(select distinct patient.patient_id AS Id,
 														   patient_identifier.identifier AS patientIdentifier,
 														   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
 														   floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
-											         	(select name from concept_name cn where cn.concept_id = o.value_coded and concept_name_type='FULLY_SPECIFIED') AS TB_Treatment_History,
+											         	 
 														   person.gender AS Gender,
 														   observed_age_group.name AS age_group,
 														   observed_age_group.sort_order AS sort_order
@@ -13,18 +14,17 @@
 									from obs o
 										
 											 INNER JOIN patient ON o.person_id = patient.patient_id 
-											  AND o.concept_id =3785
+											  AND o.concept_id =3785 and o.value_coded=1034
 											 AND patient.voided = 0 AND o.voided = 0
-											 AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
 
-                                           AND o.person_id in (
+											 
+											  AND o.person_id not in (
 												select distinct os.person_id 
 												from obs os
-												where os.concept_id = 3785 and os.value_coded in (1034,1084)
+												where os.concept_id = 	3772 and os.value_coded =2095
+												
 												AND patient.voided = 0 AND o.voided = 0
-												AND os.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
 											 )
-									
 											 
 											 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
 											 INNER JOIN person_name ON person.person_id = person_name.person_id
@@ -32,5 +32,43 @@
 											 INNER JOIN reporting_age_group AS observed_age_group ON
 											  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 											  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
-									     WHERE observed_age_group.report_group_name = 'Modified_Ages') AS HTSClients_HIV_Status
-)
+									     WHERE observed_age_group.report_group_name = 'Modified_Ages') AS HTSClients_HIV_STATUS
+					ORDER BY HTSClients_HIV_STATUS.HIV_STATUS, HTSClients_HIV_STATUS.Age)
+
+
+UNION
+
+	(SELECT patientIdentifier AS "Patient Identifier", patientName AS "Patient Name", Age , Gender, age_group, 'Relapsed Patient' AS 'TB_Treatment_History'
+							 
+					FROM
+									(select distinct patient.patient_id AS Id,
+														   patient_identifier.identifier AS patientIdentifier,
+														   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+														   floor(datediff(CAST('#endDate#' AS DATE), person.birthdate)/365) AS Age,
+											         	 
+														   person.gender AS Gender,
+														   observed_age_group.name AS age_group,
+														   observed_age_group.sort_order AS sort_order
+  
+									from obs o
+										
+											 INNER JOIN patient ON o.person_id = patient.patient_id 
+											  AND o.concept_id =3785 and o.value_coded=1084
+											 AND patient.voided = 0 AND o.voided = 0
+
+											   AND o.person_id not in (
+												select distinct os.person_id 
+												from obs os
+												where os.concept_id = 3772 and os.value_coded =2095
+												
+												AND patient.voided = 0 AND o.voided = 0
+											 )
+											 
+											 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+											 INNER JOIN person_name ON person.person_id = person_name.person_id
+											 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
+											 INNER JOIN reporting_age_group AS observed_age_group ON
+											  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+											  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+									     WHERE observed_age_group.report_group_name = 'Modified_Ages') AS HTSClients_HIV_STATUS
+					ORDER BY HTSClients_HIV_STATUS.HIV_STATUS, HTSClients_HIV_STATUS.Age)
