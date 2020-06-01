90c2253cf3767a1074b6879c7648726a682d4a6d
Mapholoho Thamae <nn2455@cumc.columbia.edu>
2020-06-01 16:28:24 +0200

    Update intakes to separate Nurse Vs Counselor

--- openmrs/apps/reports/sql/No_intakes.sql
+++ openmrs/apps/reports/sql/No_intakes.sql
@@ -7,17 +7,19 @@ FROM
 							concat(pn.given_name, ' ', pn.family_name) AS patientName
 							
 			FROM obs o
-									INNER JOIN patient_identifier p ON o.person_id = p.patient_id 
-									INNER JOIN person_name pn ON p.patient_id = pn.person_id
-									AND o.person_id not in
-									(				
-										select person_id from obs where concept_id = 2249
-									)								
-									
-									WHERE p.identifier_type = 5 
-									AND o.voided = 0
+			INNER JOIN patient_identifier p ON o.person_id = p.patient_id 
+			INNER JOIN person_name pn ON p.patient_id = pn.person_id
+			AND o.person_id not in
+			(				
+				select person_id 
+				from obs 
+				where (concept_id = 4270 and concept_id = 2397)
+				and obs_datetime <= cast(:endDate as date)
+				and voided = 0
+			)								
+			AND o.voided = 0
 			
-		) AS Patient_MissedAppointments
+		) AS no_intakes
 
 ORDER BY 2;
 
