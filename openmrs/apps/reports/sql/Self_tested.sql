4c0d22d8d11dac0b8747372522e8a1bf5d7a8a7e
Masupha <thekomasupha@gmail.com>
2021-06-23 12:43:04 +0200

    New Reports for Self Testing

---
+++ openmrs/apps/reports/sql/Self_tested.sql
@@ -0,0 +1,121 @@
+SELECT Patient_Identifier, Patient_Name, Age, Gender, age_group, HIV_Testing_Initiation  , HIV_Status
+FROM (
+(SELECT patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group, 'Self-test' AS 'HIV_Testing_Initiation'
+                          , HIV_Status, sort_order
+		FROM
+						(select distinct patient.patient_id AS Id,
+											   patient_identifier.identifier AS patientIdentifier,
+											   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+											   floor(datediff(CAST('2021-01-31' AS DATE), person.birthdate)/365) AS Age,
+											   (select name from concept_name cn where cn.concept_id = 1738 and concept_name_type='FULLY_SPECIFIED') AS HIV_Status,
+											   person.gender AS Gender,
+											   observed_age_group.name AS age_group,
+											   observed_age_group.sort_order AS sort_order
+						from obs o
+								-- HTS SELF TEST CLIENTS WITH HIV STATUS BY SEX AND AGE
+								 INNER JOIN patient ON o.person_id = patient.patient_id 
+								 AND o.concept_id = 4845 and value_coded = 4822
+								 AND patient.voided = 0 AND o.voided = 0
+								 AND o.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+								 
+								 -- HAS HIV POSITIVE RESULTS 
+								 AND o.person_id in (
+									select distinct os.person_id
+									from obs os
+									where os.concept_id = 4844 and os.value_coded = 1738
+									AND os.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+									AND patient.voided = 0 AND o.voided = 0
+								 )
+								 
+								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
+								 INNER JOIN reporting_age_group AS observed_age_group ON
+								  CAST('2021-01-31' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+						   WHERE observed_age_group.report_group_name = 'Modified_Ages'
+								 ) AS HTSClients_HIV_Status
+		ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
+		
+UNION
+(SELECT patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group, 'Self-test' AS 'HIV_Testing_Initiation'
+				, HIV_Status, sort_order
+		FROM
+						(select distinct patient.patient_id AS Id,
+											   patient_identifier.identifier AS patientIdentifier,
+											   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+											   floor(datediff(CAST('2021-01-31' AS DATE), person.birthdate)/365) AS Age,
+											   (select name from concept_name cn where cn.concept_id = 1016 and concept_name_type='FULLY_SPECIFIED') AS HIV_Status,
+											   person.gender AS Gender,
+											   observed_age_group.name AS age_group,
+											   observed_age_group.sort_order AS sort_order
+						from obs o
+								-- HTS SELF TEST CLIENTS WITH HIV STATUS BY SEX AND AGE
+								 INNER JOIN patient ON o.person_id = patient.patient_id 
+								 AND o.concept_id = 4845 and value_coded = 4822
+								 AND patient.voided = 0 AND o.voided = 0
+								 AND o.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+								 
+								 -- HAS HIV POSITIVE RESULTS 
+								 AND o.person_id in (
+									select distinct os.person_id
+									from obs os
+									where os.concept_id = 4844 and os.value_coded = 1016
+									AND os.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+									AND patient.voided = 0 AND o.voided = 0
+								 )
+								 
+								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
+								 INNER JOIN reporting_age_group AS observed_age_group ON
+								  CAST('2021-01-31' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+						   WHERE observed_age_group.report_group_name = 'Modified_Ages'
+								 ) AS HTSClients_HIV_Status
+		ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
+		
+UNION
+(SELECT patientIdentifier AS "Patient_Identifier", patientName AS "Patient_Name", Age, Gender, age_group, 'Self-test' AS 'HIV_Testing_Initiation'
+				 , HIV_Status, sort_order
+		FROM
+						(select distinct patient.patient_id AS Id,
+											   patient_identifier.identifier AS patientIdentifier,
+											   concat(person_name.given_name, ' ', person_name.family_name) AS patientName,
+											   floor(datediff(CAST('2021-01-31' AS DATE), person.birthdate)/365) AS Age,
+											   (select name from concept_name cn where cn.concept_id = 2148 and concept_name_type='FULLY_SPECIFIED') AS HIV_Status,
+											   person.gender AS Gender,
+											   observed_age_group.name AS age_group,
+											   observed_age_group.sort_order AS sort_order
+						from obs o
+								-- HTS SELF TEST CLIENTS WITH HIV STATUS BY SEX AND AGE
+								 INNER JOIN patient ON o.person_id = patient.patient_id 
+								 AND o.concept_id = 4845 and value_coded = 4822
+								 AND patient.voided = 0 AND o.voided = 0
+								 AND o.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+								 
+								 -- HAS HIV POSITIVE RESULTS 
+								 AND o.person_id in (
+									select distinct os.person_id
+									from obs os
+									where os.concept_id = 4844 and os.value_coded = 2148
+									AND os.obs_datetime BETWEEN CAST('2020-10-01' AS DATE) AND CAST('2021-01-31' AS DATE)
+									AND patient.voided = 0 AND o.voided = 0
+								 )
+								 
+								 INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+								 INNER JOIN person_name ON person.person_id = person_name.person_id
+								 INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3
+								 INNER JOIN reporting_age_group AS observed_age_group ON
+								  CAST('2021-01-31' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
+								  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
+						   WHERE observed_age_group.report_group_name = 'Modified_Ages'
+								 ) AS HTSClients_HIV_Status
+		ORDER BY HTSClients_HIV_Status.HIV_Status, HTSClients_HIV_Status.Age)
+		
+) AS HTS_Status_Detailed
+ORDER BY HTS_Status_Detailed.HIV_Testing_Initiation
+			, HTS_Status_Detailed.sort_order
+			, HTS_Status_Detailed.Gender
+			, HTS_Status_Detailed.HIV_Status
+
