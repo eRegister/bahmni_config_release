dcef32aba0ff4a36d2f8013b86f77e4d8f026d8f
lefahla <lefahla@gmail.com>
2021-06-23 18:50:30 +0200

    Quick FIx : Index Reports

--- openmrs/apps/reports/sql/egpaf/index_elicited_contacts_list.sql
+++ openmrs/apps/reports/sql/egpaf/index_elicited_contacts_list.sql
@@ -1,14 +1,24 @@
+ 
 SELECT Client_Name as "Client Name", contact_Name as "Contact Name", Contact_age as "Contact Age", Gender as "Contact Gender"
 FROM (
-
+     
+    -- List the elicited contact name and age for the Index Contact               
 SELECT Id,concat(given_name ,' ',family_name) as Client_Name, concat(firstname,' ',surname) as contact_Name,Contact_age,c_gender.contact_gender as Gender from
 (
+    -- List the elicited contact name and age for the Index Contact            
     SELECT Id, given_name , family_name, concept_id, firstname, surname,age_set.Contact_age, obs_group_id from
     (
+        -- List the elicited contact firstname and surname for Index Client
         SELECT Id, first_name_set.given_name, first_name_set.family_name, first_name_set.concept_id, firstname, surname, first_name_set.obs_group_id from
         (   
-            select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided  from obs o
-                inner join person_name pn ON o.person_id=pn.person_id 
+            -- Get elicited contact first name for the Index Client               
+            select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided
+             from obs o
+                INNER JOIN patient ON o.person_id = patient.patient_id 
+                INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                
                 AND o.voided=0
                 AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
 								                
@@ -17,10 +27,17 @@ SELECT Id,concat(given_name ,' ',family_name) as Client_Name, concat(firstname,'
 
         inner join 
         (
-            select obs_id, o.person_id, given_name, family_name, concept_id, value_text as surname, obs_group_id, o.voided  from obs o
-            inner join person_name pn ON o.person_id=pn.person_id 
-            AND o.voided=0
-            AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
+             -- Get elicited contact surname for the Index Client                   
+            select obs_id, o.person_id, given_name, family_name, concept_id, value_text as surname, obs_group_id, o.voided 
+            from obs o
+                
+                INNER JOIN patient ON o.person_id = patient.patient_id 
+                INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                 
+                AND o.voided=0
+                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
 
             where concept_id in (4762) 
             group by obs_group_id
@@ -30,9 +47,14 @@ SELECT Id,concat(given_name ,' ',family_name) as Client_Name, concat(firstname,'
             
     inner join
     (
+        -- Get the contact age for the Index Client
         select obs_id, o.person_id, value_numeric as Contact_age, o.obs_group_id as age_obs_group_id, o.voided  
         from obs o
-        inner join person_name pn ON o.person_id=pn.person_id
+            INNER JOIN patient ON o.person_id = patient.patient_id 
+            INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+            INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+            INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                 
         AND o.voided=0
         AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) 
      
@@ -54,18 +76,23 @@ SELECT Id,concat(given_name ,' ',family_name) as Client_Name, concat(firstname,'
 inner join
 
 (
+     -- Get the contact age for the Index Client
     select obs_id, o.person_id, IF(value_coded = 1088,'F','M') as contact_gender, o.obs_group_id as gender_obs_group_id, o.voided  
     from obs o
-    inner join person_name pn ON o.person_id=pn.person_id 
-    AND o.voided=0
-    AND o.value_coded in (1088,1087)
+        INNER JOIN patient ON o.person_id = patient.patient_id 
+        INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+        INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+        INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+            
+    AND o.voided=0   
     AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
     
+    AND o.value_coded in (1088,1087)
     AND o.obs_group_id in (
                 select oss.obs_group_id
-                from obs oss inner join person p ON oss.person_id=p.person_id 
+                from obs oss inner join person p ON oss.person_id = p.person_id 
                 AND oss.concept_id = 4769 
-                AND oss.voided=0 
+                AND oss.voided = 0 
                 AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
     )
 
@@ -74,7 +101,7 @@ inner join
 
 ON Contact_age.obs_group_id = c_gender.gender_obs_group_id
 group by obs_group_id 
-) as pivot
+) as INDEX_CLIENTS_CONTACTS
 group by Client_Name,Contact_age,Gender
 
 
--- openmrs/apps/reports/sql/egpaf/index_elicited_contacts_pivot.sql
+++ openmrs/apps/reports/sql/egpaf/index_elicited_contacts_pivot.sql
@@ -18,50 +18,61 @@ FROM (
 						, IF(INDEX_STATUS_DRVD_ROWS.Id IS NULL, 0, SUM(IF( ((INDEX_STATUS_DRVD_ROWS.Gender = 'M') || (INDEX_STATUS_DRVD_ROWS.Gender = 'F')), 1, 0))) as 'Total'
 						, INDEX_STATUS_DRVD_ROWS.sort_order
 			FROM (  
+                        -- List the elicited contact name,age,gender for the Index Client
                 SELECT Id,Client_Name, contact_Name, Contact_age, Gender, '' as age_group, 1 as sort_order
-                FROM (
-
+                FROM (                        
+                        -- List the elicited contact name and age for the Index Contact
                     SELECT Id,concat(given_name ,' ',family_name) as Client_Name, concat(firstname,' ',surname) as contact_Name,Contact_age,c_gender.contact_gender as Gender from
-                    (
+                    (   
+                         -- List the elicited contact firstname and surname for Index Client
                         SELECT Id,given_name , family_name, concept_id, firstname, surname,age_set.Contact_age, obs_group_id from
-                        (
+                        (   
+                            -- Get elicited contact first name for the Index Client
                             SELECT Id,first_name_set.given_name, first_name_set.family_name, first_name_set.concept_id, firstname, surname, first_name_set.obs_group_id from
                             (   
-                                select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided  from obs o
+                                 select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided  from obs o
                                     inner join person_name pn ON o.person_id=pn.person_id 
                                     AND o.voided=0
-                                    AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+                                    AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE)
                                                                     
                                     where concept_id in (4761)
-                                group by obs_group_id) as first_name_set 
+                                group by obs_group_id
+                            ) as first_name_set 
 
                             inner join 
-                            (
+                            (   
+                                -- Get elicited contact surname for the Index Client
                                 select obs_id, o.person_id, given_name, family_name, concept_id, value_text as surname, obs_group_id, o.voided  from obs o
                                 inner join person_name pn ON o.person_id=pn.person_id 
                                 AND o.voided=0
-                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
+                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE)								 
 
                                 where concept_id in (4762) 
                                 group by obs_group_id
                             ) as surname_set 
                                     ON first_name_set.obs_group_id=surname_set.obs_group_id 
-                        ) as names
+                        ) as names 
                                 
                         inner join
-                        (
+                        (   
+                            -- Get the contact age for the Index Client
                             select obs_id, o.person_id, value_numeric as Contact_age, o.obs_group_id as age_obs_group_id, o.voided  
                             from obs o
-                            inner join person_name pn ON o.person_id=pn.person_id
-                            AND o.voided=0
-                            AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) 
-                        
+                
+                                INNER JOIN patient ON o.person_id = patient.patient_id 
+                                INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                                INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                                INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                                
+                                AND o.voided=0
+                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
+
                             AND o.obs_group_id in ( 
                                         select oss.obs_group_id
                                         from obs oss inner join person p ON oss.person_id=p.person_id 
                                         AND oss.concept_id = 4769 
                                         AND oss.voided=0
-                                        AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) 
+                                        AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE) 
                             )
                                 
                             where concept_id = 4769
@@ -73,20 +84,25 @@ FROM (
 
                     inner join
 
-                    (
+                    (   
+                        -- Get the elicited contact gender for Index Client
                         select obs_id, o.person_id, IF(value_coded = 1088,'F','M') as contact_gender, o.obs_group_id as gender_obs_group_id, o.voided  
-                        from obs o
-                        inner join person_name pn ON o.person_id=pn.person_id 
-                        AND o.voided=0
+                       
+                        from obs o                
+                            INNER JOIN patient ON o.person_id = patient.patient_id 
+                            INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                            INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                            INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                            
+                            AND o.voided=0
+                            AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
                         AND o.value_coded in (1088,1087)
-                        AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
-                        
                         AND o.obs_group_id in (
                                     select oss.obs_group_id
                                     from obs oss inner join person p ON oss.person_id=p.person_id 
                                     AND oss.concept_id = 4769 
                                     AND oss.voided=0 
-                                    AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+                                    AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE)
                         )
 
                         group by obs_group_id 
@@ -94,7 +110,7 @@ FROM (
 
                     ON Contact_age.obs_group_id = c_gender.gender_obs_group_id
                     group by obs_group_id 
-                    ) as pivot
+                    ) as INDEX_CLIENTS_CONTACTS
                     group by Client_Name,Contact_age,Gender
 
 
@@ -115,30 +131,44 @@ FROM (
 						, IF(INDEX_STATUS_DRVD_COLS.Id IS NULL, 0, SUM(IF( ((INDEX_STATUS_DRVD_COLS.Gender = 'M') || (INDEX_STATUS_DRVD_COLS.Gender = 'F')), 1, 0))) as 'Total'
 						, 99 AS sort_order
 			FROM ( 
-                 --
+                        -- List the elicited contact name,age,gender for the Index Client
                 SELECT Id, Client_Name, contact_Name, Contact_age, Gender, 1 as sort_order
-                FROM (
-
-                    SELECT Id, concat(given_name ,' ',family_name) as Client_Name, concat(firstname,' ',surname) as contact_Name,Contact_age,c_gender.contact_gender as Gender from
+                FROM (                        
+                        -- List the elicited contact name and age for the Index Contact
+                     SELECT Id, concat(given_name ,' ',family_name) as Client_Name, concat(firstname,' ',surname) as contact_Name,Contact_age,c_gender.contact_gender as Gender from
                     (
+                        -- List the elicited contact firstname and surname for Index Client
                         SELECT Id, given_name , family_name, concept_id, firstname, surname,age_set.Contact_age, obs_group_id from
                         (
+                             -- Get elicited contact first name for the Index Client
                             SELECT Id, first_name_set.given_name, first_name_set.family_name, first_name_set.concept_id, firstname, surname, first_name_set.obs_group_id from
                             (   
-                                select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided  from obs o
-                                    inner join person_name pn ON o.person_id=pn.person_id 
-                                    AND o.voided=0
-                                    AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+                                select obs_id, o.person_id as Id, given_name, family_name, concept_id, value_text as firstname, obs_group_id, o.voided 
+                                from obs o 
+                                    INNER JOIN patient ON o.person_id = patient.patient_id 
+                                    INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                                    INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                                    INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                                    
+                                AND o.voided=0   
+                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)                      
                                                                     
                                     where concept_id in (4761)
-                                group by obs_group_id) as first_name_set 
+                                group by obs_group_id
+                            ) as first_name_set 
 
                             inner join 
-                            (
-                                select obs_id, o.person_id, given_name, family_name, concept_id, value_text as surname, obs_group_id, o.voided  from obs o
-                                inner join person_name pn ON o.person_id=pn.person_id 
-                                AND o.voided=0
-                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)								 
+                            ( 
+                                -- Get elicited contact surname for the Index Client
+                                select obs_id, o.person_id, given_name, family_name, concept_id, value_text as surname, obs_group_id, o.voided 
+                                from obs o 
+                                    INNER JOIN patient ON o.person_id = patient.patient_id 
+                                    INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                                    INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                                    INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                                        
+                                AND o.voided=0   
+                                AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)                       					 
 
                                 where concept_id in (4762) 
                                 group by obs_group_id
@@ -148,18 +178,23 @@ FROM (
                                 
                         inner join
                         (
+                            -- Get the contact age for the Index Client
                             select obs_id, o.person_id, value_numeric as Contact_age, o.obs_group_id as age_obs_group_id, o.voided  
-                            from obs o
-                            inner join person_name pn ON o.person_id=pn.person_id
-                            AND o.voided=0
-                            AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) 
-                        
+                            from obs o 
+                                INNER JOIN patient ON o.person_id = patient.patient_id 
+                                INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                                INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                                INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                                    
+                            AND o.voided=0   
+                            AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+                       
                             AND o.obs_group_id in ( 
                                         select oss.obs_group_id
                                         from obs oss inner join person p ON oss.person_id=p.person_id 
                                         AND oss.concept_id = 4769 
                                         AND oss.voided=0
-                                        AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE) 
+                                        AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE) 
                             )
                                 
                             where concept_id = 4769
@@ -172,19 +207,24 @@ FROM (
                     inner join
 
                     (
+                         -- Get the elicited contact gender for Index Client
                         select obs_id, o.person_id, IF(value_coded = 1088,'F','M') as contact_gender, o.obs_group_id as gender_obs_group_id, o.voided  
-                        from obs o
-                        inner join person_name pn ON o.person_id=pn.person_id 
-                        AND o.voided=0
-                        AND o.value_coded in (1088,1087)
+                        from obs o 
+                            INNER JOIN patient ON o.person_id = patient.patient_id 
+                            INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
+                            INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
+                            INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type IN (3,5) AND patient_identifier.preferred=1
+                                
+                        AND o.voided=0   
                         AND o.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
-                        
+                       
+                        AND o.value_coded in (1088,1087)
                         AND o.obs_group_id in (
                                     select oss.obs_group_id
                                     from obs oss inner join person p ON oss.person_id=p.person_id 
                                     AND oss.concept_id = 4769 
                                     AND oss.voided=0 
-                                    AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)
+                                    AND oss.obs_datetime BETWEEN CAST('#startDate#' AS DATE) AND CAST('2021-02-28' AS DATE)
                         )
 
                         group by obs_group_id 
@@ -192,7 +232,7 @@ FROM (
 
                     ON Contact_age.obs_group_id = c_gender.gender_obs_group_id
                     group by obs_group_id 
-                    ) as pivot
+                    ) as INDEX_CLIENTS_CONTACTS
                     group by Client_Name,Contact_age,Gender
 			) AS INDEX_STATUS_DRVD_COLS
 		)
