cc57faef3c893eea5dd456178cb104ab05ff83c9
seapehim <seapehim@gmail.com>
2021-07-21 12:58:15 +0200

    Appointment Status Report Corrections

--- openmrs/apps/reports/sql/current_on_art_joint_missed_appointments_list.sql
+++ openmrs/apps/reports/sql/current_on_art_joint_missed_appointments_list.sql
@@ -78,7 +78,7 @@ FROM
 						INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
 						INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
 						INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND patient_identifier.preferred=1 AND p.voided = 0
+						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND p.voided = 0
                         INNER JOIN reporting_age_group AS observed_age_group ON
 						CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 						AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
@@ -174,7 +174,7 @@ FROM
 						INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
 						INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
 						INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND patient_identifier.preferred=1 AND p.voided = 0
+						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND p.voided = 0
                         INNER JOIN reporting_age_group AS observed_age_group ON
 						  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 						  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
@@ -263,7 +263,7 @@ FROM
 					    INNER JOIN person ON person.person_id = patient.patient_id AND person.voided = 0
 						INNER JOIN person_name ON person.person_id = person_name.person_id AND person_name.preferred = 1
 						INNER JOIN patient_identifier ON patient_identifier.patient_id = person.person_id AND patient_identifier.identifier_type = 3 AND patient_identifier.preferred=1
-						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND patient_identifier.preferred=1 AND p.voided = 0
+						LEFT OUTER JOIN patient_identifier p ON p.patient_id = patient.patient_id AND p.identifier_type = 5 AND p.voided = 0
                         INNER JOIN reporting_age_group AS observed_age_group ON
 						  CAST('#endDate#' AS DATE) BETWEEN (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.min_years YEAR), INTERVAL observed_age_group.min_days DAY))
 						  AND (DATE_ADD(DATE_ADD(person.birthdate, INTERVAL observed_age_group.max_years YEAR), INTERVAL observed_age_group.max_days DAY))
